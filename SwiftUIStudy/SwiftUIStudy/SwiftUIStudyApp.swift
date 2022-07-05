6520d78843142f34fa9df75321c832c075256045
kongweifeng <kongweifeng@xueqiu.com>
2022-07-05 16:23:36 +0800

    creat project

---
+++ SwiftUIStudy/SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by kongweifeng on 2022/7/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
